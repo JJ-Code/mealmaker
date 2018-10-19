bb8f784515854114ac1f639c6c9511517a12a012
Jaymee Liu <{jaymeeliu@gmail.com}>
2018-10-18 22:24:41 -0700

    added library

--- teamStats.js
+++ teamStats.js
@@ -75,3 +75,13 @@ team.addGame('Jets', 44, 36)
 team.addGame('Ravens', 23, 41)
 
 console.log(team._games)
+
+const car = {
+    numDoors: 4,
+    isDirty: true,
+    color: 'red'
+}
+
+for (let value in car) {
+    console.log(value)
+}
